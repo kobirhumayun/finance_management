6a720eff175a33eda3e7ed2af356e649ff684698
kobirhumayun <72266367+kobirhumayun@users.noreply.github.com>
2025-10-28 13:56:00 +0600

    Use crypto RNG for OTP generation

--- server/utils/otpUtils.js
+++ server/utils/otpUtils.js
@@ -4,14 +4,17 @@
  * @param {number} length The desired length of the OTP (default: 6).
  * @returns {string} The generated OTP as a string.
  */
+const { randomInt } = require('node:crypto');
+
 const generateOtp = (length = 6) => {
     if (length <= 0) {
         throw new Error("OTP length must be a positive number.");
     }
-    const min = Math.pow(10, length - 1);
-    const max = Math.pow(10, length) - 1;
-    const otp = Math.floor(min + Math.random() * (max - min + 1));
-    return otp.toString();
+    let otp = '';
+    for (let i = 0; i < length; i += 1) {
+        otp += randomInt(0, 10).toString();
+    }
+    return otp;
 };
 
 module.exports = {
