18460c6f9074d2ffbb1ef9005630c028c2b44b7c
kobirhumayun <72266367+kobirhumayun@users.noreply.github.com>
2025-11-21 14:42:27 +0600

    Add site config and render footer social links (#169)
    * Add site config and render footer social links
    
    * Open footer social links in new tab

---
+++ client/src/config/site.js
@@ -0,0 +1,29 @@
+import { Facebook, Linkedin, Twitter, Youtube } from "lucide-react";
+
+export const siteConfig = {
+  name: "FinTrack",
+  socialLinks: [
+    {
+      name: "Facebook",
+      href: "https://facebook.com",
+      icon: Facebook,
+    },
+    {
+      name: "YouTube",
+      href: "https://youtube.com",
+      icon: Youtube,
+    },
+    {
+      name: "Twitter / X",
+      href: "https://twitter.com",
+      icon: Twitter,
+    },
+    {
+      name: "LinkedIn",
+      href: "https://linkedin.com",
+      icon: Linkedin,
+    },
+  ],
+};
+
+export default siteConfig;
